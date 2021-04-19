499f88c926b5c599638acc32d818560d7ce41805
Rofhiwa Sikhauli <rofhiwa.sikhauli@absa.africa>
2021-04-19 15:24:41 +0200

    Added some comments and fixed toolbar up button

--- app/src/main/java/co/za/giantpanda/log_in/MainActivity.java
+++ app/src/main/java/co/za/giantpanda/log_in/MainActivity.java
@@ -2,61 +2,58 @@ package co.za.giantpanda.log_in;
 
 import android.content.Context;
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-  private Context context;
+    private Context context;
 
-  public final static String FIRST_NAME = "NAME";
-  public final static String LAST_NAME = "SURNAME";
+    public final static String FIRST_NAME = "NAME";
+    public final static String LAST_NAME = "SURNAME";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    context = this;
+        context = this;
 
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    Button loginButton = findViewById(R.id.loginButton);
-    Button registerButton = findViewById(R.id.registerButton);
-    TextView loginUsername = findViewById(R.id.loginUsername);
-    TextView loginPassword = findViewById(R.id.loginPassword);
+        // Learn Android studio shortcuts here: https://developer.android.com/studio/intro/keyboard-shortcuts
+        //
 
-    setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.registerButton);
+        TextView loginUsername = findViewById(R.id.loginUsername);
+        TextView loginPassword = findViewById(R.id.loginPassword);
 
-    ActionBar actionBar = getSupportActionBar();
+        setSupportActionBar(toolbar);
 
-    if (actionBar != null) {
-      actionBar.setTitle(getString(R.string.login));
-      //actionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.login));
+        }
+
+        //This called lambda. Go check it: https://www.w3schools.com/java/java_lambda.asp
+        registerButton.setOnClickListener(view -> {
+            Intent intent = new Intent(context, Register.class);
+            intent.putExtra(FIRST_NAME, "Rofhiwa");
+            intent.putExtra(LAST_NAME, "Sikhauli");
+            startActivity(intent);
+        });
+
+        //I used lambda here as well
+        loginButton.setOnClickListener(view -> Toast.makeText(getApplicationContext(),
+                " First Name: " + loginUsername.getText().toString() +
+                        " Password: " + loginPassword.getText().toString(),
+                Toast.LENGTH_LONG).show());
     }
-    //startActivity(new Intent( MainActivity.this, Register.class));
-
-    registerButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Intent intent = new Intent(context, Register.class);
-        intent.putExtra(FIRST_NAME, "Rofhiwa");
-        intent.putExtra(LAST_NAME, "Sikhauli");
-        startActivity(intent);
-      }
-    });
-    loginButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Toast.makeText(getApplicationContext(),
-            " First Name: " + loginUsername.getText().toString() +
-                 " Password: " + loginPassword.getText().toString() ,
-            Toast.LENGTH_LONG).show();}
-    });
-
-
-  }
 }
--- app/src/main/java/co/za/giantpanda/log_in/Register.java
+++ app/src/main/java/co/za/giantpanda/log_in/Register.java
@@ -3,99 +3,81 @@ package co.za.giantpanda.log_in;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.MenuItem;
-import android.view.Menu;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import android.content.ClipData.Item;
-import androidx.appcompat.widget.Toolbar;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NavUtils;
+import androidx.appcompat.widget.Toolbar;
 
 public class Register extends AppCompatActivity {
 
-  private Context context;
+    private Context context;
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_register);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
 
-    context = this;
+        context = this;
 
-    Toolbar toolbar =findViewById(R.id.toolbar);
-    TextView firstNameTextView = findViewById(R.id.firstName);
-    TextView lastNameTextView = findViewById(R.id.lastName);
-    TextView pass_TextView = findViewById(R.id.pass_);
-    TextView UnameTextView = findViewById(R.id.Uname);
-    Button Reg_login = findViewById(R.id.Reg_login);
+        //you don't use underscores(_) on Java when naming a field/variable/methods/class
+        //Rename variable like pass_TextView to correct naming standard eg: passwordTextView
+        //Use full word no shortcuts or abbreviations, UnameTextView should be usernameTextView
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView firstNameTextView = findViewById(R.id.firstName); //Type is EditText not TextView
+        TextView lastNameTextView = findViewById(R.id.lastName);//Type is EditText not TextView
+        TextView pass_TextView = findViewById(R.id.pass_); //Type is EditText not TextView
+        TextView UnameTextView = findViewById(R.id.Uname); //Type is EditText not TextView
+        Button Reg_login = findViewById(R.id.Reg_login);
 
+        //Read this: https://source.android.com/setup/contribute/code-style#follow-field-naming-conventions
+        //And this: https://dzone.com/articles/best-practices-variable-and
 
-    setSupportActionBar(toolbar);
-    ActionBar actionBar = getSupportActionBar();
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
 
-    if (actionBar != null) {
-      actionBar.setTitle("Register");
-      actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.setTitle("Register");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Intent intent = getIntent();
+
+        if (intent.getExtras() != null) {
+            String firstName = intent.getStringExtra(MainActivity.FIRST_NAME);
+            String lastName = intent.getStringExtra(MainActivity.LAST_NAME);
+
+            firstNameTextView.setText(firstName);
+            lastNameTextView.setText(lastName);
+        }
+
+        //I also used lambda, learn and get used to it.
+        findViewById(R.id.reg_).setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(),
+                    " First Name: " + firstNameTextView.getText().toString() +
+                            " Last Name: " + lastNameTextView.getText().toString() +
+                            " Username: " + UnameTextView.getText().toString() +
+                            " Password: " + pass_TextView.getText().toString(),
+                    Toast.LENGTH_LONG).show();
+        });
+
+        //Lambda here too
+        Reg_login.setOnClickListener(view -> {
+            Intent intent1 = new Intent(context, MainActivity.class);
+            startActivity(intent1);
+        });
     }
-      //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-      //getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-    //
-    //public boolean onOptionsItemSelected( MenuItem item) {
-    //
-    //  if (item.getItemId()==android.R.id.home){
-    //      finish();
-    //  }
-    //      return super.onOptionsItemSelected(item);
-    //}
-
-
-    //startActivity(new Intent( Register.this, MainActivity.class));
-
-    Intent intent = getIntent();
-
-    if (intent.getExtras() != null) {
-      String firstName = intent.getStringExtra(MainActivity.FIRST_NAME);
-      String lastName = intent.getStringExtra(MainActivity.LAST_NAME);
-
-      firstNameTextView.setText(firstName);
-      lastNameTextView.setText(lastName);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-
-    findViewById(R.id.reg_).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        //Toast.makeText(Register.this, ((Button)view).getText().toString() + " ", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(),
-                " First Name: " + firstNameTextView.getText().toString() +
-                " Last Name: " + lastNameTextView.getText().toString() +
-                " Username: " + UnameTextView.getText().toString() +
-                " Password: " + pass_TextView.getText().toString() ,
-            Toast.LENGTH_LONG).show();
-
-        //Button button1 = findViewById(R.id.button_1);
-        //
-        //button1.setOnClickListener(new View.OnClickListener() {
-        //  @Override public void onClick(View view) {
-        //    Toast.makeText(MainActivity.this, button1.getText().toString() + " is pressed", Toast.LENGTH_SHORT).show();
-        //  }
-        //});
-
-      }
-    });
-    Reg_login.setOnClickListener(new View.OnClickListener(){
-      @Override public void onClick(View view) {
-        Intent intent = new Intent(context, MainActivity.class);
-        startActivity(intent);
-
-      }
-    });
-  }
 }
