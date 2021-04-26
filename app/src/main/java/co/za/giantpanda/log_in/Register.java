3fb95a640c7458788ef07cb993272f35840c5117
Rofhiwa Sikhauli <rofhiwa.sikhauli@hellogroup.co.za>
2021-04-26 09:03:17 +0200

    up button white color

--- app/src/main/java/co/za/giantpanda/log_in/Register.java
+++ app/src/main/java/co/za/giantpanda/log_in/Register.java
@@ -2,9 +2,13 @@ package co.za.giantpanda.log_in;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,10 +31,10 @@ public class Register extends AppCompatActivity {
         //Rename variable like pass_TextView to correct naming standard eg: passwordTextView
         //Use full word no shortcuts or abbreviations, UnameTextView should be usernameTextView
         Toolbar toolbar = findViewById(R.id.toolbar);
-        TextView firstNameTextView = findViewById(R.id.firstName); //Type is EditText not TextView
-        TextView lastNameTextView = findViewById(R.id.lastName);//Type is EditText not TextView
-        TextView pass_TextView = findViewById(R.id.pass_); //Type is EditText not TextView
-        TextView UnameTextView = findViewById(R.id.Uname); //Type is EditText not TextView
+        EditText firstNameTextView = findViewById(R.id.firstName); //Type is EditText not TextView
+        EditText  lastNameTextView = findViewById(R.id.lastName);//Type is EditText not TextView
+        EditText  pass_TextView = findViewById(R.id.pass_); //Type is EditText not TextView
+        EditText  UnameTextView = findViewById(R.id.Uname); //Type is EditText not TextView
         Button Reg_login = findViewById(R.id.Reg_login);
 
         //Read this: https://source.android.com/setup/contribute/code-style#follow-field-naming-conventions
@@ -43,6 +47,9 @@ public class Register extends AppCompatActivity {
             actionBar.setTitle("Register");
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+
 
         Intent intent = getIntent();
 
